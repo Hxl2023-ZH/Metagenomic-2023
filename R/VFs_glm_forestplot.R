a8ad8ef5b4a87e84f969ee4cfad11db11cf09b58
Hxl2023-ZH <126218017+Hxl2023-ZH@users.noreply.github.com>
2023-09-05 09:40:29 +0800

    20230905

--- R/VFs_glm_forestplot.R
+++ R/VFs_glm_forestplot.R
@@ -1,16 +1,18 @@
 
 ################################################################################
-###                             毒力因子回归分析
+###                                细菌毒力因子逻辑回归分析
 ################################################################################
 # XinLiang Hu
 # 2023-08-23
 
-setwd("C:/Users/huxin/Desktop/ARG_Vfs_glm/")
+setwd("C:/Users/huxin/Desktop/Final_result_analysys/10_ARG_Vfs_glm")
 library(dplyr)
 
 df <- read.table("tmp.txt", header=T, check.names = FALSE)
 head(df)
 
+
+#df$Group<-factor(df$Group, levels = c(1,0), labels = c("Obesity","Control")) #转换数据类型
 df$Group1<-factor(df$Group1, levels = c(0,1), labels = c("Control","Obesity")) #转换数据类型
 
 #df$Group1<-factor(df$Group1)
@@ -30,12 +32,10 @@ fit.full1 <- step(fit.full, direction = "both") #逐步回归分析
 #fit.full1 <- step(fit.full, direction = "both", scope = list(lower=Group~Bacterial_Vfs_Shannon+Bacterial_Vfs_Richness, upper=fit.full)) #逐步回归分析
 
 fit.result1<-summary(fit.full1)
-fit.full1
 fit.result1
 
 
-df1<-fit.result1$coefficients
-#df2<-confint(fit.full1)
+df1<-exp(coefficients(fit.result1)) #对结果进行指数化
 df2 <- exp(confint(fit.full1))
 df3<-cbind(df1,df2)
 df4<-data.frame(df3[-1,c(1,4,5,6)])
@@ -43,20 +43,23 @@ df4$Var<-rownames(df4)
 colnames(df4)<-c("OR","Pvalue","OR_1","OR_2","Var")
 df5<-df4[,c(5,1,2,3,4)]
 df5$OR_mean<-df5$OR
-df5$OR<-paste0(round(df5$OR,2),
+df5$OR<-paste0(round(df5$OR,4),
                "(",
-               round(df5$OR_1,2),
+               round(df5$OR_1,4),
                "~",
-               round(df5$OR_2,2),
+               round(df5$OR_2,4),
                ")")
+df5$Pvalue <- log(df5$Pvalue)
 df5$Pvalue<-round(df5$Pvalue,3)
 df5
 
-write.csv(df5,file = "Arg_VFs_forestplot_data.csv",
+write.csv(df5,file = "Arg_VFs_forestplot_data_0904.csv",
           quote = F,row.names = F)
 
-
+#绘制森林图，可跳过
 library(forestplot)
+
+#需先调整表格
 fp<-read.csv("Arg_VFs_forestplot_data.csv",header=T)
 
 forestplot(labeltext=as.matrix(fp[,1:3]),
